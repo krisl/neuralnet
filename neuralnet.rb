1ab593d418c3dc192d463e6be6b9bf21df2c4241
Aaron Lipinski <kris.lipinski@gmail.com>
2015-05-10 15:10:45 +1200

    feed forward details

--- neuralnet.rb
+++ neuralnet.rb
@@ -27,13 +27,15 @@ class Net
 end
 
 class Layer
+  attr_accessor :neurons
+
   def initialize neuron_count, num_outputs
     puts neuron_count
     @neurons = Array.new(neuron_count){ Neuron.new(num_outputs) }
   end
 
   def feed_forward prev_layer
-    @neurons.each_with_index{|neuron, i| neuron.feed_forward(prev_layer)}
+    @neurons.each_with_index{|neuron, i| neuron.feed_forward(prev_layer, i)}
   end
 
   def set_output_values output_vals
@@ -57,8 +59,21 @@ class Neuron
     @output = value
   end
 
-  def feed_forward layer
+  def feed_forward prev_layer, i
+    sum = prev_layer.neurons.reduce(0){|r, n| r += n.get_output_weighted_for(i) }
+    @output = Neuron::transfer_function sum
+  end
 
+  def get_output_weighted_for i
+    @output * @connections[i].weight
+  end
+
+  def self.transfer_function thing
+    Math::tanh thing
+  end
+
+  def self.transfer_function_diriv thing
+    1.0 - thing * thing
   end
 end
 
@@ -81,4 +96,6 @@ target_vals = []
 net.back_prop target_vals
 
 result_vals = net.get_results
+
+puts result_vals
 
