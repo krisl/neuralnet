7b5222d7c28c43103a4137968e28e37f35cb7f68
Aaron Lipinski <kris.lipinski@gmail.com>
2019-07-19 13:49:10 +1200

    wip

--- neuralnet.rb
+++ neuralnet.rb
@@ -18,11 +18,37 @@ class Net
   end
 
   def back_prop target_vals
+    puts "Calc overall net errors"
+    output_layer = @layers.last
+    error = output_layer.neurons.each_with_index.sum do |neuron, i|
+      delta = target_vals[i] - neuron.output
+      delta * delta
+    end
 
+    error /= output_layer.size - 1
+    @error = Math.sqrt error
+
+    puts "output layer gradients"
+    output_layer.each_with_index{|neuron, i| neuron.calc_output_gradient(target_vals[i])}
+
+    puts "hidden layer grandients"
+    @layers.reverse_each.with_index do |layer, i|
+      next if layer == 0 || layer == @layers.size - 1 #skip output, input layers
+      next_layer = @layers[i + 1]
+      layer.neurons.each{|neuron| neuron.calc_hidden_gradient(next_layer)}
+    end
+
+    puts "update connection weights"
+    @layers.reverse_each.with_index do |layer, i|
+      next if layer == 0 || layer == @layers.size - 1 #skip output, input layers
+      prev_layer = @layers[i - 1]
+      layer.neurons.each_with_index{|neuron, i| neuron.update_input_weights(prev_layer, i)}
+    end
   end
 
   def get_results
-
+    output_layer = @layers.last
+    output_layer.neurons.map{|neuron| neuron.output }
   end
 end
 
@@ -46,9 +72,19 @@ class Layer
   def size
     @neurons.size
   end
+
+  def calc_dow connections
+    @neurons.each_with_index.sum do |neuron, i|
+      neuron.gradient * connections[i].weight
+    end
+  end
 end
 
 class Neuron
+  attr_reader :output, :gradient
+  @@eta = 0.15
+  @@alpha = 0.5
+
   def initialize num_outputs
     puts "making a neuron with #{num_outputs} outputs"
     @connections = Array.new(num_outputs){ Connection.new }
@@ -75,10 +111,38 @@ class Neuron
   def self.transfer_function_diriv thing
     1.0 - thing * thing
   end
+
+  def calc_output_gradient target_val
+    delta = target_val - @output
+    @gradient = delta * Neuron::transfer_function_diriv(@output)
+  end
+
+  def calc_hidden_gradient next_layer
+    dow = next_layer.dow connections
+    @gradient = dow * Neuron::transfer_function(@output)
+  end
+
+  def get_output_delta_weight_for i
+    @connections[i].delta_weight
+  end
+
+  def set_delta_weight delta, i
+    @connections[i].delta_weight = delta
+    @connections[i].weight += delta
+  end
+
+  def update_input_weights prev_layer, i
+    prev_layer.neurons.each do |neuron|
+      old_delta = neuron.get_output_delta_weight_for i
+      new_delta = eta * neuron.output * @gradient * alpha * old_delta
+
+      neuron.delta_weight(new_delta)
+    end
+  end
 end
 
 class Connection
-  attr_accessor :weight
+  attr_accessor :weight, :delta_weight
 
   def initialize
     @weight = Random.new.rand
